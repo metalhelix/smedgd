159aa785849f145d536a6e5357bc2ea3428e0033
Jim V <jfv@stowers.org>
2013-04-08 16:21:30 -0500

    initial cap

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem "friendly_id", "~> 4.0.9" # Note: You MUST use 4.0.9 or greater for Rails 3.
 
 gem "high_voltage"
 
+# Deploy with Capistrano
+gem 'capistrano'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
@@ -37,8 +39,6 @@ gem "high_voltage"
 # Use unicorn as the app server
 # gem 'unicorn'
 
-# Deploy with Capistrano
-# gem 'capistrano'
 
 # To use debugger
 # gem 'debugger'
---
+++ config/deploy.rb
@@ -0,0 +1,53 @@
+require 'bundler/capistrano'
+
+set :application, 'smedgd'
+set :repository, 'git@github.com:metalhelix/smedgd.git'
+
+set :domain, 'smedgdkc03'
+
+set :scm, :git
+set :scm_username, 'vlandham'
+set :branch, 'master'
+set :scm_verbose, true
+set :ssh_options, { :forward_agent => true }
+set :repository_cache, "git_cache"
+set :deploy_via, :remote_cache
+
+set :deploy_to, "/var/www/#{application}"
+set :user, 'deployer'
+set :use_sudo, false
+
+
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+
+namespace :deploy do
+  task :start do
+  end
+
+  task :stop do
+  end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
